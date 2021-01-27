bfa18babc0da45fbec9126d7af13a5f07bbad54b
Jacob Zhang <Jzhang5850@gmail.com>
2021-01-27 13:46:03 -0500

    BBBAutonomous set up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBBAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBBAutonomous.java
@@ -1,9 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.HardwareBIGBRAINBOTS.HardwareBIGBRAINBOTS;
 
-public class BBBAutonomous {
-    HardwareBIGBRAINBOTS robot   = new HardwareBIGBRAINBOTS();   // Use BIGBRAINBOTS's hardware
+public class BBBAutonomous extends LinearOpMode {
+    HardwareBIGBRAINBOTS robot = new HardwareBIGBRAINBOTS();   // Use BIGBRAINBOTS's hardware
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(this.hardwareMap);
 
+        
+
+    }
 }
